c83bbc86426b873f5ad622da6c8e3813f94bdfa6
YellarajuSivarla <yellaraju.sivarla_cs21@gla.ac.in>
2024-10-01 18:17:28 +0530

    Add files via upload

---
+++ wlp.java
@@ -0,0 +1,14 @@
+//Java Program to demonstate the use of if statement.
+public class wlp {
+    public static void main(String[] args) {
+        // defining an 'age' variable
+        int age = 20;
+        // checking the age
+        if (age > 21) {
+            System.out.print("Age is greater than 20");
+        } else {
+            System.out.println("above 20 ");
+        }
+
+    }
+}
